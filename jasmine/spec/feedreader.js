6f50d2e4723ff1ddf01c67704395dcb7f519ca06
Haku <hakuhal.snow@gmail.com>
2019-12-30 22:09:41 +0800

    finish all this, submit for once, and wait for result.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,18 +84,38 @@ $(function() {
         });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries',function () {
+         beforeEach(function (done) {
+              loadFeed(0,function () {
+                    done();
+              });
+         });
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        it('loadFeed works',function (done) {
+             expect($('.feed .entry')).toExist();
+             done();
+        })
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New feed selection',function () {
 
+        beforeEach(function (done) {
+            loadFeed(0,function () {
+                done();
+            });
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         it('when loadFeed worked, the content will changed',function (done) {
+             expect($('.feed').first().html()).not.toBeEmpty();
+             done();
+         })
+        });
 }());
